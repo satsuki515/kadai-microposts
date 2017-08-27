2afafb84133fd4173e6ddcbcfbf796ced0bb2a00
Satsuki Hasegawa <hasegawasatsuki3@gmail.com>
2017-08-27 07:36:01 +0000

    add favorites

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -39,6 +39,12 @@ class UsersController < ApplicationController
     counts(@user)
   end
   
+   def favings
+    @user = User.find(params[:id])
+    @favings = @user.favings.page(params[:page])
+    counts(@user)
+  end
+  
   private
   
   def user_params
